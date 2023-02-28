0a5e4c77de33df2263b1ce36afa4d4bf542c125b
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-02-28 02:41:41 -0600

    form changes

--- src/contact/ContactForm.js
+++ src/contact/ContactForm.js
@@ -3,9 +3,30 @@ import { motion } from "framer-motion";
 import React from "react";
 
 const ContactForm = () => {
-    const [state, handleSubmit] = useForm("xeqwkelw");
+    const [state, handleSubmit] = useForm(process.env.REACT_APP_FORMSPREE_KEY);
     if (state.succeeded) {
-        return <p>Thanks for joining!</p>;
+        return (
+            <motion.p
+                id="thanks"
+                initial={{ scale: 0, opacity: 0 }}
+                whileInView={{ scale: 1, opacity: 1 }}
+                viewport={{
+                    once: false,
+                    amount: 0.75,
+                    margin: "-100px 0px 0px 0px",
+                }}
+                transition={{
+                    delay: 0,
+                    duration: 0.5,
+                    type: "ease",
+                }}
+            >
+                Thanks for reaching out{" "}
+                <span style={{ color: "var(--pastel-green)" }}>
+                    Your message has been sent
+                </span>
+            </motion.p>
+        );
     }
 
     return (
