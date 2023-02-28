cbc94e7307770f62fc54ee65e7b1d999ed967495
noko12312 <42224565+noko12312@users.noreply.github.com>
2023-02-28 02:23:11 -0600

    added contact form with formspree

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,20 +1,34 @@
 import { motion } from "framer-motion";
 import React from "react";
 import { staggerCont, textVar } from "../utils/motion";
+import "./contact.css";
+import ContactForm from "./ContactForm";
 
 const Contact = () => {
     return (
         <section id="contact" className="pageContainer">
             <motion.div
-                className="about"
+                className="contact"
                 variants={staggerCont}
                 initial="hidden"
                 whileInView="show"
                 viewport={{ once: false, amount: 0.25 }}
             >
-                <motion.div variants={textVar(0.25)} className="sectionTitle">
-                    Under Construction
+                <motion.div
+                    initial={{ opacity: 0, y: 100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    viewport={{
+                        once: false,
+                        amount: 1,
+                        margin: "-50px 0px 0px 0px",
+                    }}
+                    className="sectionTitle"
+                    transition={{ duration: 0.5, delay: 0 }}
+                    exit={{ opacity: 0, y: 0 }}
+                >
+                    Contact
                 </motion.div>
+                <ContactForm />
             </motion.div>
         </section>
     );
---
+++ src/contact/ContactForm.js
@@ -0,0 +1,115 @@
+import { useForm, ValidationError } from "@formspree/react";
+import { motion } from "framer-motion";
+import React from "react";
+
+const ContactForm = () => {
+    const [state, handleSubmit] = useForm("xeqwkelw");
+    if (state.succeeded) {
+        return <p>Thanks for joining!</p>;
+    }
+
+    return (
+        <div>
+            <form id="contactForm" onSubmit={handleSubmit}>
+                <motion.label
+                    htmlFor="email"
+                    initial={{ scale: 0, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{
+                        once: false,
+                        amount: 0.75,
+                        margin: "-100px 0px 0px 0px",
+                    }}
+                    transition={{
+                        delay: 0,
+                        duration: 0.5,
+                        type: "ease",
+                    }}
+                >
+                    Email
+                </motion.label>
+                <motion.input
+                    id="email"
+                    type="email"
+                    name="email"
+                    initial={{ scale: 0, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{
+                        once: false,
+                        amount: 0.75,
+                        margin: "-100px 0px 0px 0px",
+                    }}
+                    transition={{
+                        delay: 0,
+                        duration: 0.5,
+                        type: "ease",
+                    }}
+                />
+                <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                />
+                <motion.label
+                    htmlFor="message"
+                    initial={{ scale: 0, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{
+                        once: false,
+                        amount: 0.75,
+                        margin: "-100px 0px 0px 0px",
+                    }}
+                    transition={{
+                        delay: 0,
+                        duration: 0.5,
+                        type: "ease",
+                    }}
+                >
+                    Message
+                </motion.label>
+                <motion.textarea
+                    id="message"
+                    rows="10"
+                    name="message"
+                    initial={{ scale: 0, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{
+                        once: false,
+                        amount: 0.75,
+                        margin: "-100px 0px 0px 0px",
+                    }}
+                    transition={{
+                        delay: 0,
+                        duration: 0.5,
+                        type: "ease",
+                    }}
+                />
+                <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                />
+                <motion.button
+                    type="submit"
+                    disabled={state.submitting}
+                    initial={{ scale: 0, opacity: 0 }}
+                    whileInView={{ scale: 1, opacity: 1 }}
+                    viewport={{
+                        once: false,
+                        amount: 0.75,
+                        margin: "-100px 0px 0px 0px",
+                    }}
+                    transition={{
+                        delay: 0,
+                        duration: 0.5,
+                        type: "ease",
+                    }}
+                >
+                    Send
+                </motion.button>
+            </form>
+        </div>
+    );
+};
+
+export default ContactForm;
